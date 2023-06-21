4efd22d78f65276ae5398b3729548b7c97eb06c0
Quaqva <sanek15056@gmail.com>
2023-06-21 21:42:29 +0400

    feat: add text setter for base class

--- rs-css/src/components/base-component.ts
+++ rs-css/src/components/base-component.ts
@@ -36,6 +36,10 @@ export default class BaseComponent<T extends HTMLElement> {
     return this.element.outerHTML;
   }
 
+  public set textContent(value: string) {
+    this.element.innerText = value;
+  }
+
   public append(...elements: (HTMLElement | BaseComponent<HTMLElement>)[]): void {
     elements.forEach((element) => {
       if (element instanceof BaseComponent) this.element.append(element.element);
