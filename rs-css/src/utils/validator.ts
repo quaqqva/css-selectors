22262276bc18ede553095f624f98dda0d9ff8f1a
Quaqva <sanek15056@gmail.com>
2023-06-26 23:10:00 +0400

    feat: add selector validator template

---
+++ rs-css/src/utils/validator.ts
@@ -0,0 +1,5 @@
+import { TableItemData } from '../types/level-types';
+
+export default function validateSelector(markup: TableItemData[], selector: string, solution: string): boolean {
+  return true;
+}
