f4c353f451d10ac76b414b2b919943a2d9c0c110
Quaqva <sanek15056@gmail.com>
2023-07-04 01:59:31 +0400

    fix: markdown parser targeted  char between opened and closed tags

--- rs-css/src/utils/parse-markdown.ts
+++ rs-css/src/utils/parse-markdown.ts
@@ -1,6 +1,6 @@
 // This function converts ``` of md to tag <code> and ** to tag <b>
 export default function parseMarkdown(md: string): string {
-  const codeRegex = /```.+```/g;
+  const codeRegex = /```[^`]+```/g;
   const codeReplacer = (match: string) => `<code>${match.substring(3, match.length - 3)}</code>`;
   let result = md.replace(codeRegex, codeReplacer);
 
